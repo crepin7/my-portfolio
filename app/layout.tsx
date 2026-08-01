3aebd8718a9892d3cc0bc9dc0157177db88c5004
crepin <aziamadjicrepin@gmail.com>
2026-08-01 07:20:29 +0000

    feat: add

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
       <head>
 	<Script
           async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?c>
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2200633323957306"
           crossOrigin="anonymous"
           strategy="afterInteractive"
       />
